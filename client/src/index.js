3074d2875ff10d7ed2308f30ba140859e5c049e0
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-10-23 22:17:21 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './Redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
